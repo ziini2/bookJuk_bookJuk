401227f13ac2b3f8a8dcbcd3544adfe876843d67
WalkingYM <ssrtomoya@naver.com>
2024-10-22 17:04:14 +0900

    rent, rentcontroller

---
+++ src/main/java/com/itwillbs/bookjuk/controller/RentController.java
@@ -0,0 +1,23 @@
+package com.itwillbs.bookjuk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class RentController {
+	
+	@GetMapping("/rent")
+	public String rent() {
+		log.info("RentController rent()");
+		return "/rent/rent";
+	}
+	
+	
+	
+	
+}

30ab45a9001e5d3b840ce22472c43513f1b6ec6c
WalkingYM <ssrtomoya@naver.com>
2024-10-23 18:21:53 +0900

    Merge branch 'dev' of https://github.com/ziini2/bookJuk_bookJuk.git into dev_ltw

--- src/main/java/com/itwillbs/bookjuk/controller/rent/RentController.java
+++ src/main/java/com/itwillbs/bookjuk/controller/rent/RentController.java
@@ -1,4 +1,4 @@
-package com.itwillbs.bookjuk.controller;
+package com.itwillbs.bookjuk.controller.rent;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

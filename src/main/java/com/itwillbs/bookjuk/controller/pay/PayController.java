8ef18c1c645eb0278ab847aef5646b224ce7c352
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-10-25 20:51:21 +0900

    [2024.10.25 제가인] 결제 내역 페이지

---
+++ src/main/java/com/itwillbs/bookjuk/controller/pay/PayController.java
@@ -0,0 +1,21 @@
+package com.itwillbs.bookjuk.controller.pay;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PayController {
+	
+	@GetMapping("/pay_list")
+	public String payList() {
+
+		return "/pay/pay_list";
+	}
+	
+	@GetMapping("/adwa")
+	public String adwa() {
+
+		return "/pay/adwa";
+	}
+	
+}
---
+++ src/main/resources/static/js/pay/pay.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$('.input-daterange').datepicker({
+    format: 'dd-mm-yyyy',
+    autoclose: true
+});
+
+});

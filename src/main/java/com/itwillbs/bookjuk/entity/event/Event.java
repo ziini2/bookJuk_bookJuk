deda8697fc6de160360b2b2f5943a726790d0426
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2024-10-27 21:07:43 +0900

    [2024.10.27.김주완]테이블 정의서에 맞게 entity 수정

--- src/main/java/com/itwillbs/bookjuk/entity/event/Event.java
+++ src/main/java/com/itwillbs/bookjuk/entity/event/Event.java
@@ -19,30 +19,42 @@ import lombok.ToString;
 @ToString
 public class Event {
 
+	// 이벤트 아이디
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "eventId", nullable = false)
-	private int eventId;
+	private Integer eventId;
 	
+	// 이벤트 제목
 	@Column(name = "eventTitle", length = 255, nullable = false)
 	private String eventTitle;
 	
+	// 이벤트 내용
 	@Column(name = "eventContent", columnDefinition = "TEXT", nullable = false)
 	private String eventContent;
 	
+	// 이벤트 상태(시작 전, 진행 중, 종료, 중지)
 	@Column(name = "eventStatus", length = 10, nullable = false)
 	private String eventStatus;
 	
+	// 이벤트 유형
 	@Column(name = "eventType", length = 255, nullable = false)
 	private String eventType;
 	
-	@Column(name = "startEvent", nullable = false)
-	private Timestamp startEvent;
+	// 이벤트 시작 날짜
+	@Column(name = "startEventDate", nullable = false)
+	private Timestamp startEventDate;
 	
-	@Column(name = "endEvent", nullable = false)
-	private Timestamp endEvent;
+	// 이벤트 종료 날짜
+	@Column(name = "endEventDate", nullable = false)
+	private Timestamp endEventDate;
 	
-	@Column(name = "eventManager", length = 50, nullable = false)
+	// 이벤트 생성 날짜
+	@Column(name = "endCreationDate", nullable = false)
+	private Timestamp endCreationDate;
+	
+	// 담당자
+	@Column(name = "eventManager", length = 255, nullable = false)
 	private String eventManager;
 	
 	

9290f23f2e8b2b750ef387add7f0395068e6967a
son883 <apdlf1@protonmail.com>
2024-10-28 12:39:57 +0900

    [2024.10.28] 유저,지점 뷰완성

--- src/main/java/com/itwillbs/bookjuk/controller/customer/CutsomerController.java
+++ src/main/java/com/itwillbs/bookjuk/controller/customer/CutsomerController.java
@@ -9,14 +9,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class CutsomerController {
 	
 	@GetMapping("/store/store_list")
-	public String store() {
+	public String storeList() {
 		return "customer/store";
 	}
 	
 	@GetMapping("/store/store_info")
-	public String getMethodName() {
+	public String storeInfo() {
 		return "customer/store_info";
 	}
 	
+	@GetMapping("/user/user_list")
+	public String userList() {
+		return "customer/user";
+	}
 	
+	@GetMapping("/user/user_info")
+	public String userInfo() {
+		return "customer/user_info";
+	}
 }

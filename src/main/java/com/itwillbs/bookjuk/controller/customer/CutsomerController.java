605d823825c8d274b78018b6b2b91b74ebe20bb7
user <user@192.168.137.247>
2024-10-29 13:41:07 +0900

    [2024.10.29 손승경] 지도 진행중

--- src/main/java/com/itwillbs/bookjuk/controller/customer/CutsomerController.java
+++ src/main/java/com/itwillbs/bookjuk/controller/customer/CutsomerController.java
@@ -2,28 +2,27 @@ package com.itwillbs.bookjuk.controller.customer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class CutsomerController {
 	
-	@GetMapping("/store/store_list")
+	@GetMapping("/admin/store/store_list")
 	public String storeList() {
 		return "customer/store";
 	}
 	
-	@GetMapping("/store/store_info")
+	@GetMapping("/admin/store/store_info")
 	public String storeInfo() {
 		return "customer/store_info";
 	}
 	
-	@GetMapping("/user/user_list")
+	@GetMapping("/admin/user/user_list")
 	public String userList() {
 		return "customer/user";
 	}
 	
-	@GetMapping("/user/user_info")
+	@GetMapping("/admin/user/user_info")
 	public String userInfo() {
 		return "customer/user_info";
 	}

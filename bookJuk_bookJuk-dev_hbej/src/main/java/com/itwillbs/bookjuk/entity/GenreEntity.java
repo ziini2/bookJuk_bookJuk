28c4ecc31d49346d9447d7d4c5a0178c16ec013f
ziini2 <rksvusgo123@naver.com>
2024-10-30 18:00:12 +0900

    Add files via upload

---
+++ bookJuk_bookJuk-dev_hbej/src/main/java/com/itwillbs/bookjuk/entity/GenreEntity.java
@@ -0,0 +1,33 @@
+package com.itwillbs.bookjuk.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GenreEntity {
+	
+	// 장르ID
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long genreId;
+		
+	// 장르이름
+	@Column(nullable = false)
+	private String genreName;
+
+}

7b768b6d4c0da0ff9eb6536c35cf91072dc9f57d
user <user@192.168.137.247>
2024-10-31 09:48:41 +0900

    [2024.10.31 손승경] 지점목록 진행중

--- src/main/java/com/itwillbs/bookjuk/entity/StoreEntity.java
+++ src/main/java/com/itwillbs/bookjuk/entity/StoreEntity.java
@@ -1,5 +1,7 @@
 package com.itwillbs.bookjuk.entity;
 
+import java.sql.Timestamp;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -8,27 +10,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
+@Table(name = "store")
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class StoreEntity {
 
 	// 지점번호
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
 	private Long storeCode;
 
 	// 지점이름
+	@Column(nullable = false)
 	private String storeName;
 
 	// 지점 전체주소
+	@Column(nullable = false)
 	private String storeLocation;
 
 	// 지점 동주소
@@ -36,22 +45,28 @@ public class StoreEntity {
 	private String storeLocation2;
 
 	// 지점 전화번호
+	@Column(nullable = false)
 	private String storeTel;
 
 	// 지점 대표자명
+	@Column(nullable = false)
 	private String storeRepresent;
 
 	// 지점 이메일
+	@Column(nullable = false)
 	private String storeEmail;
 
 	// 지점 사업자등록번호
+	@Column(nullable = false)
 	private String storeRegiNum;
 
 	// 지점 등록일
 	@CreationTimestamp
-	private String storeRegiDate;
+	@Column(nullable = false)
+	private Timestamp storeRegiDate;
 
 	// 지점 정보 수정일
 	@UpdateTimestamp
-	private String storeUpdateDate;
+	@Column(nullable = false)
+	private Timestamp storeUpdateDate;
 }

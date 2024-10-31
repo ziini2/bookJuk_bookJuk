363f7157db6dc36b2147d5d4a08f0a4becd2cf29
LEEBOHYUN <100012474+bohyun86@users.noreply.github.com>
2024-10-31 17:41:23 +0900

    24/10/30 자료조회 화면 4개 디자인 완성

--- src/main/java/com/itwillbs/bookjuk/config/SecurityConfig.java
+++ src/main/java/com/itwillbs/bookjuk/config/SecurityConfig.java
@@ -57,9 +57,9 @@ public class SecurityConfig {
                         "/loginCheck", "/actuator/**", "/logs/**", "/find",
                         "/sendSmsCodeTest", "/codeValidate", "/verifySmsCode").permitAll()
                 .requestMatchers("/phone").hasAnyRole("INACTIVE") //소셜로그인 회원은 전화번호 입력하지않으면 계속해서 전화번호 입력창으로 리다이렉트!
-                .requestMatchers("/admin").hasRole("ADMIN")
+//                .requestMatchers("/admin").hasRole("ADMIN")
                 .requestMatchers("/test").hasAnyRole("ADMIN", "USER") //여기에 로그인 된사람만 할수있는 페이지 추가
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
         );
 
         //권한이 없는 페이지에 접근할 때 설정

07ced261426fb37d8fcc749f78df57f878073c85
geonokwon <furitstore1@naver.com>
2024-11-02 22:33:10 +0900

    [24.11.02] 주석 및 사용하지 않는 코드 정리

--- src/main/java/com/itwillbs/bookjuk/controller/login/LoginController.java
+++ src/main/java/com/itwillbs/bookjuk/controller/login/LoginController.java
@@ -2,7 +2,6 @@ package com.itwillbs.bookjuk.controller.login;
 
 
 import com.itwillbs.bookjuk.util.SecurityUtil;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

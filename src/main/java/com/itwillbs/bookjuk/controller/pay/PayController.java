f257043d84160ca88f18744f4f109f1e4f2684db
wprk7 <wprk7@DESKTOP-QFE6555>
2024-11-04 16:58:08 +0900

    [2024.11.04 제가인] 엔티티 추가

--- src/main/java/com/itwillbs/bookjuk/controller/pay/PayController.java
+++ src/main/java/com/itwillbs/bookjuk/controller/pay/PayController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PayController {
 	
-	@GetMapping("/pay_list")
+	@GetMapping("/admin/pay_list")
 	public String payList() {
 
 		return "/pay/pay_list";
 	}
 	
-	@GetMapping("/refund")
+	@GetMapping("/admin/refund")
 	public String refund() {
 
 		return "/pay/refund";
---
+++ src/main/java/com/itwillbs/bookjuk/entity/pay/Cart.java
@@ -0,0 +1,45 @@
+package com.itwillbs.bookjuk.entity.pay;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table (name = "cart")
+public class Cart {
+	
+	//장바구니ID
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cartItemID;
+	
+	//수량
+	@Column(nullable = false)
+	private int itemQuantity;
+	
+	//장바구니생성일자
+	@Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private LocalDateTime cartDate;
+	
+	//포인트거래ID
+	@Column(nullable = true)
+	private Long pointPayID;
+	
+	//유저 번호
+	@Column(nullable = false)
+	private Long userNum;
+}

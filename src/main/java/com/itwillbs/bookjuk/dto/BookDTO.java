e342141c8dd07885ddc2b5c7dba8a9e609c57821
ziini2 <rksvusgo123@naver.com>
2024-11-15 05:44:37 +0900

    [24.11.15 - 은진] 도서리스트 및 페이지네이션 진행

--- src/main/java/com/itwillbs/bookjuk/dto/BookDTO.java
+++ src/main/java/com/itwillbs/bookjuk/dto/BookDTO.java
@@ -12,31 +12,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookDTO {
 
-	
 	private String isbn; // ISBN 번호
-	
-	//isbn 으로 불러올 값들
-	 private Long bookNum;
-	private String bookImage; //책이미지 URL
+
+	// isbn 으로 불러올 값들
+	private Long bookNum;
+	private Long booksId;
+	private String bookImage; // 책이미지 URL
 	private String bookName; // 도서 이름
 	private String author; // 저자
 	private String publish; // 출판사
 	private LocalDate publishDate; // 출판일
 	private String story; // 책 소개
-	private Long genreId; //장르ID
-	private Long storeCode; //지점코드
+	private Long genreId; // 장르ID
+	private Long storeCode; // 지점코드
 	private BookStatus bookStatus; // 도서상태
-	private Timestamp bookDate; //입고일
-	private Long rentMoney; //대여금액
-	private Long rentCount; //대여횟수
-	private Boolean rentStatus; //대여현황
-	
-	
+	private Timestamp bookDate; // 입고일
+	private Long rentMoney; // 대여금액
+	private Long rentCount; // 대여횟수
+	private Boolean rentStatus; // 대여현황
+
 }
